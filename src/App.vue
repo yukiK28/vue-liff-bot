95570dbdf523c9cb999b4e1226573e5b2ad34b71
Yuki Kadekawa <y-kadekawa@protosolution.co.jp>
2020-12-02 18:08:04 +0900

    開発環境構築

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <div class="liff-data">
+      <table>
+        <tr>
+          <th>OS</th>
+          <td>{{ os }}</td>
+        </tr>
+        <tr>
+          <th>Language</th>
+          <td>{{ language }}</td>
+        </tr>
+        <tr>
+          <th>LIFF SDK Version</th>
+          <td>{{ sdkVersion }}</td>
+        </tr>
+        <tr>
+          <th>LINE Version</th>
+          <td>{{ lineVersion }}</td>
+        </tr>
+        <tr>
+          <th>isInClient</th>
+          <td>{{ isInClient }}</td>
+        </tr>
+        <tr>
+          <th>isLoggedIn</th>
+          <td>{{ isLoggedIn }}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+// inputFormをComponentとして設定
+// import InputForm from '@/components/InputForm.vue'
+import { liff } from '@/components/utils/liff';
+
+export default {
+  name: 'app',
+  components: {
+    // InputForm
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+    console.log(liff)
+    liff.init({
+      liffId: '1655189215-9ejqWwgd'
+    })
+    .then(() => {
+      this.loggedIn = liff.isLoggedIn();
+    })
+    .catch((err) => {
+      // 失敗したときの処理
+    });
+  },
+  computed: {
+    os() {
+      return liff.getOS()
+    },
+    language() {
+      return liff.getLanguage()
+    },
+    lineVersion() {
+      return liff.getLineVersion()
+    },
+    sdkVersion() {
+      return liff.getVersion()
+    },
+    isInClient() {
+      return liff.isInClient()
+    },
+    isLoggedIn() {
+      return liff.isLoggedIn()
+    }
+  },
+  methods: {
+    // useLiff(id){
+
+    // }
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+// import path from "path";
+const fs = require('fs')
+module.exports = {
+    devServer: {
+        https: {
+            key: fs.readFileSync('./localhost+1-key.pem'),
+            cert: fs.readFileSync('./localhost+1.pem')
+        },
+        public: 'https://192.168.142.109:8080/'
+        // public: 'https://172.20.10.4:8080/'
+    }
+}
